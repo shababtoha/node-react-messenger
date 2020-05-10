3d69bcb4cde2f3181a2ae8980ee450f097a00d63
shababtoha <shabab.toha@gmail.com>
2020-05-11 00:05:10 +0600

    asdas

--- server.js
+++ server.js
@@ -39,8 +39,10 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
+server.applyMiddleware({ app });
 
-server.listen({ port: process.env.PORT || 4000 }).then(({ url,  subscriptionsUrl }) => {
+
+app.listen({ port: process.env.PORT || 4000 }).then(({ url,  subscriptionsUrl }) => {
    console.log(`🚀 Server ready at ${url}`);
    console.log(`🚀 Subscriptions ready at ${subscriptionsUrl}`)
 });
